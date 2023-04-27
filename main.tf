9a58b67d1198990fa6b617851aa93f0bd193595b
yago <yagopalmas@gmail.com>
2023-04-27 10:55:34 +0200

    azure para cdp

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "random_integer" "vm_num" {
-  min = 1
-  max = 10
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = "${var.resource_group_name_prefix}-cdp"
@@ -89,43 +84,45 @@ resource "azurerm_network_interface" "my_terraform_NIC" {
   }
 }
 
-#Create Network Interfaces
-resource "azurerm_network_interface" "my_terraform_NIC2" {
-  name                = "NIC-Private"
+#Create Network Interfaces master
+resource "azurerm_network_interface" "tf_nic_master" {
+  name                = "NIC-Master"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_configuration {
-    name                          = "my_nic_config-${random_integer.vm_num.id}"
+    name                          = "my_nic_config-master"
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-#Create Network Interfaces
-resource "azurerm_network_interface" "my_terraform_NIC3" {
-  name                = "NIC-Private3"
+#Create Network Interfaces workers
+resource "azurerm_network_interface" "tf_nic_workers" {
+  count               = 3
+  name                = "NIC-Private-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_configuration {
-    name                          = "my_nic_config-3"
+    name                          = "my_nic_config-${count.index}"
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
+
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "tf_nsg_at_NIC" {
   network_interface_id      = azurerm_network_interface.my_terraform_NIC.id
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 # Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "tf_nsg_at_NIC2" {
-  network_interface_id      = azurerm_network_interface.my_terraform_NIC2.id
+resource "azurerm_network_interface_security_group_association" "tf_nsg_master_NIC" {
+  network_interface_id      = azurerm_network_interface.tf_nic_master.id
   network_security_group_id = azurerm_network_security_group.tf_nsg_worker.id
 }
-
 # Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "tf_nsg_at_NIC3" {
-  network_interface_id      = azurerm_network_interface.my_terraform_NIC3.id
+resource "azurerm_network_interface_security_group_association" "tf_nsg_worker_NIC" {
+  count                     = 3
+  network_interface_id      = element(azurerm_network_interface.tf_nic_workers.*.id, count.index)
   network_security_group_id = azurerm_network_security_group.tf_nsg_worker.id
 }
 
@@ -142,7 +139,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "tf_dns_net_link" {
 }
 
 # Create (and display) an SSH key
-resource "tls_private_key" "example_ssh" {
+resource "tls_private_key" "cmhost_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
@@ -175,22 +172,20 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
 
   admin_ssh_key {
     username   = "adm_pue"
-    public_key = tls_private_key.example_ssh.public_key_openssh
+    public_key = tls_private_key.cmhost_ssh.public_key_openssh
   }
 }
-# Create virtual machine master-1
-resource "azurerm_linux_virtual_machine" "my_terraform_vm2" {
-  name                         = "master-1"
+# Create virtual machine master
+resource "azurerm_linux_virtual_machine" "my_terraform_master" {
+  name                         = "master"
   location                     = azurerm_resource_group.rg.location
   resource_group_name          = azurerm_resource_group.rg.name
-  network_interface_ids        = [azurerm_network_interface.my_terraform_NIC2.id]
+  network_interface_ids        = [azurerm_network_interface.tf_nic_master.id]
   size                         = "Standard_D2s_v3"
   proximity_placement_group_id = azurerm_proximity_placement_group.ppg.id
-  priority                     = "Spot"
-  eviction_policy              = "Deallocate"
 
   os_disk {
-    name                 = "master-1-Disk"
+    name                 = "masterDisk"
     caching              = "ReadWrite"
     storage_account_type = "StandardSSD_LRS"
   }
@@ -202,13 +197,47 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm2" {
     version   = "latest"
   }
 
-  computer_name                   = "master-1"
+  computer_name                   = "master"
   admin_username                  = "adm_pue"
   disable_password_authentication = true
 
   admin_ssh_key {
     username   = "adm_pue"
-    public_key = tls_private_key.example_ssh.public_key_openssh
+    public_key = tls_private_key.cmhost_ssh.public_key_openssh
+  }
+}
+# Create virtual machine workers
+resource "azurerm_linux_virtual_machine" "my_terraform_workers" {
+  count                        = 3
+  name                         = "worker-${count.index}"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  network_interface_ids        = [element(azurerm_network_interface.tf_nic_workers.*.id, count.index)]
+  size                         = "Standard_DS1_v2"
+  proximity_placement_group_id = azurerm_proximity_placement_group.ppg.id
+  priority                     = "Spot"
+  eviction_policy              = "Deallocate"
+
+  os_disk {
+    name                 = "worker-${count.index}-Disk"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.5"
+    version   = "latest"
+  }
+
+  computer_name                   = "worker-${count.index}"
+  admin_username                  = "adm_pue"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "adm_pue"
+    public_key = tls_private_key.cmhost_ssh.public_key_openssh
   }
 }
 
@@ -227,7 +256,7 @@ resource "azurerm_virtual_machine_data_disk_attachment" "tf_cmhsot_attach_disk" 
   lun                = "10"
   caching            = "ReadWrite"
 }
-resource "azurerm_managed_disk" "tf_master1_disk" {
+resource "azurerm_managed_disk" "tf_master_disk" {
   name                 = "master1-disk1"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
@@ -237,41 +266,26 @@ resource "azurerm_managed_disk" "tf_master1_disk" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "tf_master1_attach_disk" {
-  managed_disk_id    = azurerm_managed_disk.tf_master1_disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.my_terraform_vm2.id
+  managed_disk_id    = azurerm_managed_disk.tf_master_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.my_terraform_master.id
   lun                = "10"
   caching            = "ReadWrite"
 }
 
+resource "azurerm_managed_disk" "tf_worker_disk" {
+  count                = 3
+  name                 = "worker-${count.index}-disk1"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "StandardSSD_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 50
+}
 
-# Create virtual machine master-2
-resource "azurerm_linux_virtual_machine" "my_terraform_vm3" {
-  name                         = "master-2"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name          = azurerm_resource_group.rg.name
-  network_interface_ids        = [azurerm_network_interface.my_terraform_NIC3.id]
-  size                         = "Standard_D2s_v3"
-  proximity_placement_group_id = azurerm_proximity_placement_group.ppg.id
-
-  os_disk {
-    name                 = "master3Disk"
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
-
-  source_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7.5"
-    version   = "latest"
-  }
-
-  computer_name                   = "master-2"
-  admin_username                  = "adm_pue"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "adm_pue"
-    public_key = tls_private_key.example_ssh.public_key_openssh
-  }
+resource "azurerm_virtual_machine_data_disk_attachment" "tf_worker_attach_disk" {
+  count              = 3
+  managed_disk_id    = element(azurerm_managed_disk.tf_worker_disk.*.id, count.index)
+  virtual_machine_id = element(azurerm_linux_virtual_machine.my_terraform_workers.*.id, count.index)
+  lun                = "10"
+  caching            = "ReadWrite"
 }
